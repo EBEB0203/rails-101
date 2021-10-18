69e6be8167dc85e0eea96c5f7053ef6792b30420
Xu EB <xuyubin@jourdeness.com.tw>
2021-10-18 17:35:59 +0800

    show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,10 +5,13 @@ class GroupsController < ApplicationController
   def new
   @groups = Group.new
   end
+  def show
+  @group = Group.find(params[:id])
+  end
   def create
   @group = Group.new(group_params)
   @group.save
-  
+
     redirect_to groups_path
  end
 
