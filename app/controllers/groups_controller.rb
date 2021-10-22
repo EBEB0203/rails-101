79c678ad6fed7647f7092e5227ace55c8c79a9a8
Xu EB <xuyubin@jourdeness.com.tw>
2021-10-22 11:18:26 +0800

    post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -47,7 +48,7 @@ class GroupsController < ApplicationController
      redirect_to root_path, alert: "You have no permission."
    end
  end
- 
+
  def group_params
    params.require(:group).permit(:title, :description)
  end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user  #belongs_to 属于
   belongs_to :group #belongs_to 属于
+  validates :content, presence: true #標題不得為空
 end
