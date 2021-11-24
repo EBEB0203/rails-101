e70ed725e0311b82cf81862e60b2517135d97229
Xu EB <xuyubin@jourdeness.com.tw>
2021-11-24 18:00:14 +0800

    判斷是否為群組成員

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ActiveRecord::Base
  belongs_to :user #belongs_to 属于
  has_many :posts  #has_many 拥有很多
  validates :title, presence: true #標題限制 空白表單無法儲存
+ has_many :group_relationships
+ has_many :members, through: :group_relationships, source: :user #through通過 主要建立在多對多關聯資料庫時使用
 end
