609bc76a44a4f9e0649ac41b681b38c286871c45
Xu EB <xuyubin@jourdeness.com.tw>
2021-11-24 15:57:25 +0800

    使用scope 代替order by 排序

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user  #belongs_to 属于
   belongs_to :group #belongs_to 属于
   validates :content, presence: true #標題不得為空
+  scope :recent, -> { order("created_at DESC")} #執行recent時會拿到ActiveRecord::Relation的物件
 end
